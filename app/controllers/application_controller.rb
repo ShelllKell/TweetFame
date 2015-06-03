0632dddaabde97fdca3fb348b3ccafa558f69b37
Shelby Kelly <Shelby@Shelbys-MacBook-Air.local>
2015-06-03 10:22:36 -0600

    trying to make 500 error go away

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,34 +4,34 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   # before_action :ensure_current_user
-  before_action :verify_bowtie_user
-  before_action :verify_profile_exists
-
-  private
-  
-  def current_bowtie_user_id
-    request.headers['HTTP_X_BOWTIE_USER_ID']
-  end
-
-  def current_bowtie_user_plan
-    request.headers['HTTP_X_BOWTIE_USER_PLAN']
-  end
-
-  def current_user_profile
-    Profile.find_by bowtie_user_id: current_bowtie_user_id
-  end
-
-  def verify_profile_exists
-    unless current_user_profile
-      render json: { status: 'error', detail: 'profile-required' } and return false
-    end
-  end
-
-  def verify_bowtie_user
-    unless current_bowtie_user_id
-      render json: { status: 'error', detail: 'session-required' } and return false
-    end
-  end
+  # before_action :verify_bowtie_user
+  # before_action :verify_profile_exists
+  #
+  # private
+  # 
+  # def current_bowtie_user_id
+  #   request.headers['HTTP_X_BOWTIE_USER_ID']
+  # end
+  #
+  # def current_bowtie_user_plan
+  #   request.headers['HTTP_X_BOWTIE_USER_PLAN']
+  # end
+  #
+  # def current_user_profile
+  #   Profile.find_by bowtie_user_id: current_bowtie_user_id
+  # end
+  #
+  # def verify_profile_exists
+  #   unless current_user_profile
+  #     render json: { status: 'error', detail: 'profile-required' } and return false
+  #   end
+  # end
+  #
+  # def verify_bowtie_user
+  #   unless current_bowtie_user_id
+  #     render json: { status: 'error', detail: 'session-required' } and return false
+  #   end
+  # end
 
 
 
